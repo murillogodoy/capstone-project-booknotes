7805e7b6ad228ff38ebf66b0dbe26922a3dcc1cd
Murillo Godoy <murillexy@gmail.com>
2025-10-06 14:30:01 -0300

    prep for deploy

--- api.js
+++ api.js
@@ -10,12 +10,8 @@ app.use(bodyParser.json());
 dotenv.config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    ssl: { rejectUnauthorized: false }
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === "production" ? {rejectUnauthorized: false } : false,
 });
 
 pool.connect();
